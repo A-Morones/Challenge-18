fe5dd8dccceab4179334716afb1f51265e4d3e09
Adalberto Morones <adalbertomorones@yahoo.com>
2024-09-13 21:25:13 -0500

    adding code.

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+                }
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ],
+        thoughts
+    }
+)

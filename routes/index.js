b40fa4ddaa4691fa28de54ef5f3e7ffeb40ce331
Adalberto Morones <adalbertomorones@yahoo.com>
2024-09-18 22:26:42 -0500

    finished code

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require('express').Router(); 
+const apiRoutes = require('./api-routes');
+router.use('/api',apiRoutes);
+router.use((req, res)=>{
+    return res.status(404).send('Not found');
 
-router.use('/api', apiRoutes);
-router.use((req, res) => res.send('Not Found!'));
+});
 
 module.exports = router;
